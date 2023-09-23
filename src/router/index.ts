fbd9f2d370488404c7f5240f57ae436c6ad3d96d
doom-9 <110001160+doom-9@users.noreply.github.com>
2023-09-23 13:05:27 +0800

    test: 测试

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory, Router } from 'vue-router';
 import routes from './routes';
 
 const router: Router = createRouter({
-  history: createWebHistory('/'),
+  history: createWebHistory('/mobile'),
   routes: routes,
 });
 
